48fb088f73897455faa34266b0c504e4e9e69db8
“bimo” <1371639183@qq.com>
2018-11-10 19:53:41 +0800

    init django

--- Django_use/urls.py
+++ Django_use/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import IndexHandler
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexHandler.as_view(), name='index'),
 ]
